f8b1474b2fb637c82ed69fa498076a3921cfb786
Artem Erusov <erusovav@gmail.com>
2025-05-06 11:00:46 +0300

    Set up the architecture

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,10 @@
+import { BoardsListPage } from '@/features/boards-list';
+
 export function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <div>Hello World</div>
+      <BoardsListPage />
+    </>
+  );
 }
